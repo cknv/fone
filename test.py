4649580ea76812aebed886e0f4e5f5a37a540043
Esben Sonne <esbensonne@gmail.com>
2014-05-02 21:23:28 +0200

    Renamed folder to fone.

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import unittest
-import fonos
+import fone
 
 
 class TestTranslation(unittest.TestCase):
     def test_simple_translate(self):
-        assert fonos.translate('abc') == ['alfa', 'bravo', 'charlie']
+        assert fone.translate('abc') == ['alfa', 'bravo', 'charlie']
 
     def test_simple_parse(self):
-        assert fonos.parse('alfa bravo charlie') == ['a', 'b', 'c']
+        assert fone.parse('alfa bravo charlie') == ['a', 'b', 'c']
 
 if __name__ == '__main__':
     unittest.main()
